a559cd45c76e6c2e4d4aa43593876eb9eac14045
yoomss <iove0103@naver.com>
2024-09-26 14:50:05 +0900

    그래프 그리기 수정

--- md_algorithm/r_visualization/visualization_boxplot.R
+++ md_algorithm/r_visualization/visualization_boxplot.R
@@ -14,12 +14,12 @@ summary(gh_data)
 summary(kosha_data)
 
 #boxplot 그래프 그리기
-box <- boxplot(gh_data$공사규모, kosha_data$공사규모, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 공사 규모", col = c("#F2E64D","#19396d"))
-box <- boxplot(gh_data$발생시간, kosha_data$발생시간, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 발생시간", col = c("#F2E64D","#19396d"))
-box <- boxplot(gh_data$근무경력, kosha_data$근무경력, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 근무경력", col = c("#F2E64D","#19396d"))
-box <- boxplot(gh_data$나이, kosha_data$근무경력, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 나이", col = c("#F2E64D","#19396d"))
-box <- boxplot(gh_data$월별, kosha_data$월별, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 월별", col = c("#F2E64D","#19396d"))
-box <- boxplot(gh_data$요일별, kosha_data$요일별, names = c("비사고데이터", "사고데이터"), main = "비사고 vs 사고데이터 요일별", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$공사규모, kosha_data$공사규모, names = c("비사고데이터", "사고데이터"), main = "공사규모 BOXPLOT", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$발생시간, kosha_data$발생시간, names = c("비사고데이터", "사고데이터"), main = "발생시간 BOXPLOT", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$근무경력, kosha_data$근무경력, names = c("비사고데이터", "사고데이터"), main = "근무경력 BOXPLOT", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$나이, kosha_data$근무경력, names = c("비사고데이터", "사고데이터"), main = "나이 BOXPLOT", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$월별, kosha_data$월별, names = c("비사고데이터", "사고데이터"), main = "월별 BOXPLOT", col = c("#F2E64D","#19396d"))
+box <- boxplot(gh_data$요일별, kosha_data$요일별, names = c("비사고데이터", "사고데이터"), main = "요일별 BOXPLOT", col = c("#F2E64D","#19396d"))
 
 #boxplot 통계값
 box_stats <- box$stats 
@@ -33,23 +33,42 @@ text(x = c(1.5, 2.3), y = box_stats[5,], labels = round(box_stats[5,], 2), col =
 ###### 히스토그램 그리기 ######
 
 #histgram 그리기 plot = FALSE는 histogram을 그리지 않고 데이터만 계산
-gh_hist = hist(gh_data$공사규모, plot = FALSE)
-kosha_hist = hist(kosha_data$공사규모, plot = FALSE)
+#breaks <- seq(1, 7, by=1)
+gh_hist = hist(gh_data$근무경력, plot = FALSE)
+kosha_hist = hist(kosha_data$근무경력,plot = FALSE)
 
 #ylim : y축 범위, adjustcolor : 불투명도
-plot(gh_hist, col=adjustcolor("#F2E64D", alpha=0.8), ylim=c(0,65000), main = "공사규모 비교")
-plot(kosha_hist, col=adjustcolor("#19396d", alpha=0.7), add = TRUE)
+plot(gh_hist, col=adjustcolor("#F2E64D", alpha=1),ylim=c(0, 65000),add=TRUE)
+plot(kosha_hist, col=adjustcolor("#19396d", alpha=0.7), xlab="", main = "근무경력 histogram",  ylab="빈도",xaxt = "n")
 
 # 범례 추가 cex: FONT SIZE, toright : 범례 위치
-legend("topright",legend=c("비사고데이터","사고데이터터"),fill=c("#F2E64D","#19396d"),cex = 0.6)
+legend("topright",legend=c("비사고데이터","사고데이터터"),fill=c("#F2E64D","#19396d"),cex = 0.8)
 
+
+#공사규모 x축 레이블 추가
+axis(1, at = c(1,2,3,4), labels=c("9명이하","10~99명이하","100~499명이하","500명이하"), cex.axis = 0.8)
+
+#발생시간 x축 레이블 추가
+axis(1, at = c(1,2,3,4,5,6,7), labels=c("0~6시","6~8시","8~12시","12~13시","13~18시","18~21시","21~24시"), cex.axis = 0.8)
+
+#근무경력 x축 레이블 추가
+axis(1, at = c(1,2,3,4,5,6,7), labels=c("1년미만","1~2년미만","2~3년미만","3~4년미만","4~5년미만","5~10년미만","10년이상"), cex.axis = 0.8)
+
+#나이 x축 레이블 추가
+axis(1, at = c(1,2,3,4,5,6,7,8,9,10), labels=c("18세미만","19~24세이하","25~29세이하","30~34세이하","35~39세이하","40~44세이하","45~49세이하","50~54세이하","55~59세이하","60세이상"), cex.axis = 0.8)
+
+#월별이 x축 레이블 추가
+axis(1, at = c(1,2,3,4,5,6,7,8,9,10,11,12), labels=c("1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"), cex.axis = 0.8)
+
+#요일별 x축 레이블 추가
+axis(1, at = c(1,2,3,4,5,6,7), labels=c("월","화","수","목","금","토","일"), cex.axis = 0.8)
 
 
 #######bar그래프 그리기 ############
 
 # 비사고 데이터와 사고 데이터의 발생시간 값 빈도 계산
-gh_freq <- table(gh_data$발생시간)         
-kosha_freq <- table(kosha_data$발생시간)
+gh_freq <- table(gh_data$나이)         
+kosha_freq <- table(kosha_data$나이)
 
 # 비사고 데이터와 사고 데이터에서 발생한 고유한 발생시간 값을 중복 없이, 오름차순으로 정렬한 값
 # sort : 정렬 unique : 중복된 값 제거 
@@ -71,13 +90,14 @@ kosha_freq_full[names(kosha_freq)] <- kosha_freq
 combined_data <- rbind(gh_freq_full, kosha_freq_full)
 
 # 두 데이터를 나란히 그리기
-barplot(combined_data, 
+bp <- barplot(combined_data, 
         beside = TRUE,                    # 막대를 나란히 배치
         col = c("#F2E64D", "#19396d"),    # 각각의 색상 지정
-        main = "발생시간 빈도 비교",
-        ylim = c(0,65000),
+        main = "나이별 barplot",
+        ylim = c(0,50000),
         ylab = "빈도", 
-        xlab = "발생시간")
+        xlab = "나이별",
+        xaxt = "n") #x축 레이블 없애기
 
 # 범례 추가
 legend("topright", 
@@ -85,3 +105,22 @@ legend("topright",
        fill = c("#F2E64D", "#19396d"), 
        cex = 0.8)
 
+
+#1: x축, 2:y축, colMeans : 중앙에 배치
+#공사규모
+axis(1, at=colMeans(bp), labels=c("9명이하","10~99명이하","100~499명이하","500명이하"), cex.axis = 0.8)
+
+#발생시간
+axis(1, at=colMeans(bp), labels=c("0~6시","6~8시","8~12시","12~13시","13~18시","18~21시","21~24시"), cex.axis = 0.8)
+
+#근무경력 x축 레이블 추가
+axis(1, at=colMeans(bp), labels=c("1년미만","1~2년미만","2~3년미만","3~4년미만","4~5년미만","5~10년미만","10년이상"), cex.axis = 0.8)
+
+#월별이 x축 레이블 추가
+axis(1, at=colMeans(bp), labels=c("1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"), cex.axis = 0.8)
+
+#나이 x축 레이블 추가
+axis(1, at=colMeans(bp), labels=c("18세미만","19~24세이하","25~29세이하","30~34세이하","35~39세이하","40~44세이하","45~49세이하","50~54세이하","55~59세이하","60세이상"), cex.axis = 0.8)
+
+#요일별 x축 레이블 추가
+axis(1, at=colMeans(bp), labels=c("월","화","수","목","금","토","일"), cex.axis = 0.8)
