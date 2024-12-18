bdc24f63194d9e9f423a5e0b3a9687a0ed596d0e
yoomss <iove0103@naver.com>
2024-12-18 16:10:03 +0900

    xgboost, lightgbm 추가

--- md_algorithm_mongo/variable_selection.py
+++ md_algorithm_mongo/variable_selection.py
@@ -3,6 +3,9 @@ from scipy.stats import ttest_ind
 from scipy.stats import mannwhitneyu
 from statsmodels.multivariate.manova import MANOVA
 from sklearn.ensemble import RandomForestClassifier
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
 
 
 non_accidents_df = pd.read_csv("./md_algorithm_mongo/data/non_accidents_preprocessing.csv")
@@ -41,7 +44,7 @@ for var in variables:
 
         # 결과 출력
         print(f"변수: {var}")
-        print(f"  U-statistic: {stat:.2f}, p-value: {p:.3f}")
+        print(f"U-statistic: {stat:.2f}, p-value: {p:.3f}")
         if p < 0.05:
             print("유의미한 차이가 있습니다.")
         else:
@@ -75,4 +78,45 @@ importances = rf.feature_importances_
 for var, imp in zip(variables, importances):
     print(f"{var}: 중요도 = {imp:.3f}")
 
-#나이, 근무경력, 발생시간, 발생월
+# [머신러닝] 
+# LightGBM : Tree1이 예측하고 남은 잔차들을 Tree2, Tree3를 차례대로 지나가면서 줄여가는 것을 부스팅
+# 각 Tree들을 weak learner라고 보면 된다. 가중치 부여를 통해 오류를 개선 
+# 경사하강법 이용 즉, 손실함수를 최소화 하는 방향성을 갖고 가중치 값을 업데이트
+
+print(combined_df.head())
+x = combined_df.drop(columns=["사고여부"])
+y = combined_df["사고여부"]
+
+#데이터 분리
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# x_train : 입력 데이터  y_train:해당 데이터 라벨
+train_data = lgb.Dataset(X_train, label=y_train)
+
+#모델학습
+params = {
+        'objective': 'binary', # 이진분류 사고1, 비사고0
+    'boosting_type': 'gbdt', # 일부 트리를 무작위로 제외해 과적합 방지
+    'metric': 'binary_error', # 이진 분류에서의 오류율(실제값과 다른 비율)
+    'verbose': -1 #출력 로그 최소화
+}
+
+# num_boost_round : 생성할 트리 개수 
+lgb_model = lgb.train(params, train_data, num_boost_round=150)
+
+# feature_importance : 변수 중요도 가져오는 함수 
+# gain: 해당 변수가 트레에서 정보 이득을 얼마나 많이 가져오는지 측정 => 높은 값일 수록 모델이 해당 변수를 많이 사용
+importance = lgb_model.feature_importance(importance_type='gain')
+feature_names = X.columns
+
+for name, imp in zip(feature_names, importance):
+    print(f"{name}: 중요도 = {imp:.3f}")
+
+
+#모델 학습
+xgb_model = xgb.XGBRFClassifier()
+xgb_model.fit(X_train, y_train)
+
+#get_score(importance_type="gain") : 각 변수의 중요도 반환
+xgb_importance = xgb_model.get_booster().get_score(importance_type="gain")
+print("XGBoost 중요도 : ", xgb_importance)
